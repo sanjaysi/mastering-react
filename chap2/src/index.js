5c3a25d17ac452947e23653880e3c73ed8beeb3e
unknown <Sanjay.Singh@ATMECSINLT-011.ATMECS.CORP>
2016-09-10 20:23:37 +0530

    es6 - propTypes, defaultProps, state, bind, onChange

---
+++ chap2/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MultipleProps extends React.Component {
+  // within class it should be static
+  // Better approach is to define it outside class
+  /*
+  static propTypes = {
+      fname: React.PropTypes.string.isRequired,
+      lname: React.PropTypes.string
+  };
+  */
+
+  // getInitialState in ES6 - should be used in constructor()
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: 'I am default message'
+    };
+    this.updateMessage = this.updateMessage.bind(this);
+  }
+  updateMessage(e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+  render() {
+    var fname = this.props.fname,
+        lname = this.props.lname;
+
+    return(
+      <div>
+        <div>Hello: {fname} {lname}</div>
+        <div>Message: {this.state.message}</div>
+        <input
+          type='text'
+          onChange={this.updateMessage} />
+      </div>
+    );
+  }
+}
+
+// Better approach
+MultipleProps.propTypes = {
+    fname: React.PropTypes.string.isRequired,
+    lname: React.PropTypes.string
+};
+
+// setting defaultProps
+MultipleProps.defaultProps = {
+  lname: 'singh'
+}
+
+ReactDOM.render(<MultipleProps fname="sanjay" />, document.getElementById('container'));
